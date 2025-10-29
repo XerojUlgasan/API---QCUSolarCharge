d152af2f28f1e49a56145cad881401287fec325b
XerojUlgasan <xerojulgasan@gmail.com>
2025-10-29 18:38:48 +0800

    Add alertHistory deletion and improve report removal
    Added deletion of device records from the alertHistory collection. Improved report deletion by querying based on location and removing matching documents. This enhances cleanup of device-related data across multiple collections.

--- utils/deleteDeviceRecords.js
+++ utils/deleteDeviceRecords.js
@@ -1,4 +1,4 @@
-const { deleteDoc, doc } = require("firebase/firestore")
+const { deleteDoc, getDocs, query, collection, where, doc } = require("firebase/firestore")
 const db = require("./connectToFirebase")
 const getId = require("./getIds")
 
@@ -28,12 +28,23 @@ const deleteRecords = async (devId) => {
             collectionName: "reports",
             field: "device_id",
             ids: null
+        },{
+            collectionName: "alertHistory",
+            field: "device_id",
+            ids: null
         }
     ]
 
+    const q = query(collection(db, "reports"), where("location", "==", devId))
+    const reportIds = await (await getDocs(q)).docs.map(docc => docc.id)
+
+    reportIds.forEach(async (id) => {
+        deleteDoc(doc(db, "reports", id))
+    })
+
     toDelete.forEach(async (coll) => {
         coll.ids = await getId(coll.collectionName, coll.field, devId)
-
+        
         coll.ids.forEach(async (id) => {
             deleteDoc(doc(db, coll.collectionName, id))
         })
